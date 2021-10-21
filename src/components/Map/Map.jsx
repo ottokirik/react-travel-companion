ee71ced954139a8ca4bcb7e24f8a0f7edb26760b
Petrov Kirill <ottokirik@gmail.com>
2021-10-21 16:08:48 +0300

    Added google map without functionallity

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,3 +1,25 @@
-export function Map() {
-  return <div>M</div>;
-}
+import { useMediaQuery } from '@material-ui/core';
+import GoogleMapReact from 'google-map-react';
+import { useStyles } from './styles';
+
+export const Map = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(min-width: 600px)');
+
+  const coordinates = { lat: 0, lng: 0 };
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_KEY }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={''}
+        onChange={''}
+        onChildClick={''}
+      ></GoogleMapReact>
+    </div>
+  );
+};
